2fdf88824385a0909cf09d83398cc9051489c497
Kavya Kavuri <kk1069@scarletmail.rutgers.edu>
2022-03-23 02:25:00 -0400

    added race plot, year filter and color palette changed

--- dash_app.py
+++ dash_app.py
@@ -1,6 +1,7 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
+from raceplotly.plots import barplot
 
 app = Dash(__name__)
 
@@ -19,7 +20,24 @@ def get_countries_options():
     global df_countries
     return list(df_countries.apply(lambda x: {"label": x[1],"value":x[0]},axis=1))
 
+def display_raceplot():
+    global df
+    race_df = df.groupby(['CountryName','CountryCode','Year']).sum()
+    race_df.reset_index( drop=False, inplace=True )
+    race_df.reindex(['CountryName','CountryCode','Year','LUCF','agriculture','allsectors',
+        'electricity and heat production','energy','gaseous fuel consumption','liquid fuel consumption',
+        'manufacturing industries and construction','other sectors, excluding residential buildings and commercial and public services',
+        'residential buildings and commercial and public services','solid fuel consumption','transport'], axis=1)
+    my_raceplot = barplot(race_df,  item_column='CountryName', value_column='allsectors', time_column='Year')
+    fig = my_raceplot.plot(item_label = 'Top 10 countries', value_label = 'Emissions (tonnes)', frame_duration = 800)
+    fig.update_layout(height=700)
+    return fig
 
+def get_marks():
+    marks = {}
+    for x in range(1960,2017,4):
+        marks[x] = str(x)
+    return marks
 
 app.layout = html.Div([
     html.Div(
@@ -41,6 +59,11 @@ app.layout = html.Div([
                      # style={'width': "60%"}
                      ),
         dcc.Graph(id="choropleth_graph",figure={}),
+        dcc.RangeSlider(1960, 2016, 4, 
+            id='year_range_slider',
+            marks=get_marks(),
+            value=[1990, 2014],  
+            tooltip={"placement": "bottom", "always_visible": True}),
         html.H1('Yearly Emissions (Thousand Metric Ton CO2eq)'),
         dcc.Dropdown(id="slct_country",
                      options=get_countries_options(),
@@ -49,20 +72,22 @@ app.layout = html.Div([
                      ),
         dcc.Graph(id="country_line_chart",figure={}),
         html.H2("Sector Wise Emissions"),
-        dcc.Graph(id="pie_chart",figure={}),
+        dcc.Graph(id="sunburst_chart",figure={}),
+        dcc.Graph(id="raceplot",figure=display_raceplot()),
     ])
 ])
 
 ## Callback to update choropleth map according to gas selected
 @app.callback(
     Output("choropleth_graph", "figure"), 
-    Input("slct_gas", "value"))
+    [Input("slct_gas", "value"),
+    Input("year_range_slider","value")])
 
-def display_choropleth(gas_selected):
+def display_choropleth(gas_selected,year_range):
     global df
     # df = px.data.election() # replace with your own data source
     # geojson = px.data.election_geojson()
-    df3 = df[df['Gas']==gas_selected].groupby(['CountryName','CountryCode']).mean()
+    df3 = df[(df['Gas']==gas_selected)&(df['Year']>=year_range[0])&(df['Year']<=year_range[1])].groupby(['CountryName','CountryCode']).mean()
     df3.reset_index( drop=False, inplace=True )
     df3.reindex(['CountryName','CountryCode','Year','LUCF','agriculture','allsectors',
                         'electricity and heat production','energy','gaseous fuel consumption',
@@ -74,11 +99,11 @@ def display_choropleth(gas_selected):
                     color="allsectors", # encode colors according to allsectors column
                     hover_name="CountryName", # column to add to hover information
                     hover_data = {
-                    'CountryName':False,
-                    'allsectors': True,
-                    'CountryCode': False
+                        'CountryName':False,
+                        'allsectors': True,
+                        'CountryCode': False
                     },
-                    color_continuous_scale=px.colors.sequential.Plasma, width=1800, height=900)
+                    color_continuous_scale='RdYlGn_r', width=1800, height=900)
     fig.update_geos(projection_type="orthographic")
     fig.update_layout(clickmode='event+select')
     # fig.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
@@ -90,9 +115,10 @@ def display_choropleth(gas_selected):
     Output("country_line_chart", "figure"),
     [Input("slct_country", "value"),
     Input("slct_gas","value"),
-    Input('choropleth_graph', 'selectedData')])
+    Input('choropleth_graph', 'selectedData'),
+    Input("year_range_slider","value")])
 
-def update_line_chart(countries_selected,gas_selected, selected_data):
+def update_line_chart(countries_selected,gas_selected, selected_data,year_range):
     global df
     # print("dcc data: ", dcc_data)
     # print("countries selected from choropleth: ",type(selected_data))
@@ -106,7 +132,7 @@ def update_line_chart(countries_selected,gas_selected, selected_data):
             countries_selected.append(item['location'])
 
     
-    mask = (df['CountryCode'].isin(countries_selected)) & (df['Gas']==gas_selected)
+    mask = (df['CountryCode'].isin(countries_selected)) & (df['Gas']==gas_selected) & (df['Year']>=year_range[0]) & (df['Year']<=year_range[1])
     fig = px.line(df[mask], x="Year", y="allsectors", color='CountryName',width=900, height=500)
     return fig
 
@@ -127,12 +153,14 @@ def update_line_chart(countries_selected,gas_selected, selected_data):
 
 #     return {'countries':countries_selected}
 
+# Sunburst chart or Donut chart
 @app.callback(
-    Output('pie_chart','figure'),
+    Output('sunburst_chart','figure'),
     [Input("slct_country", "value"),
-    Input('choropleth_graph', 'selectedData')])
+    Input('choropleth_graph', 'selectedData'),
+    Input("year_range_slider","value")])
 
-def update_pie_chart(countries_selected,selected_data):
+def update_sunburst_chart(countries_selected,selected_data,year_range):
     global df_sunburst
     # print("calling pie chart thing")
 
@@ -144,10 +172,15 @@ def update_pie_chart(countries_selected,selected_data):
         for item in selected_data['points']:
             countries_selected.append(item['location'])
 
-    mask = df_sunburst['CountryCode'].isin(countries_selected)
-    fig = px.sunburst(df_sunburst[mask], path=['CountryName', 'Gas', 'Sector'], values='Emissions', width=600, height=600)
+    mask = df_sunburst['CountryCode'].isin(countries_selected) & (df_sunburst['Year']>=year_range[0]) & (df_sunburst['Year']<=year_range[1])
+    fig = px.sunburst(df_sunburst[mask], 
+                        path=['CountryName', 'Gas', 'Sector'], 
+                        values='Emissions',
+                        color_continuous_scale=px.colors.sequential.algae,
+                        width=600, 
+                        height=600)
+    fig.update_traces(textinfo="label+percent root")
     return fig
-
 
 
 
